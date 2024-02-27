ebb354de1b3e627630d2b42a057a7a03acbf66f5
Eialist <elsinoree@gmail.com>
2024-02-27 19:07:51 +0100

    fixed presentation

--- src/components/PresentationComponent.jsx
+++ src/components/PresentationComponent.jsx
@@ -17,17 +17,18 @@ export const PresentationComponent = () => {
           lineHeight: "2",
           fontSize: "1.3rem",
         }}>
-        Mitt namn är Erica Eklund och jag är alldeles snart klar med min
-        Frontend Developer-utbildning. Min bakgrund som mellanstadielärare har
-        givit mig värdefulla egenskaper som tålamod, kommunikation och
-        anpassningsförmåga, vilka jag tror kan vara användbara som utvecklare.
-        Utöver det brinner för webbgränssnitt, användarupplevelse och pedagogik.
-        Jag är lösningsorienterad och får saker gjort. Jag har kunskap inom
-        Frontend-utveckling, inklusive HTML, CSS, Bootstrap, Sass, JavaScript,
-        React, jQuery, GitHub, API:er, Typescript, UX-design och agila
-        arbetsmetoder. Dessutom har jag viss erfarenhet inom backend-utveckling.
-        Jag är en engagerad problemlösare som brinner särskilt för React, CSS
-        och design.
+        Jag heter Erica Eklund och jag står inför avslutningen av min utbildning
+        som Frontend Developer. Med en bakgrund som mellanstadielärare har jag
+        förvärvat värdefulla egenskaper såsom tålamod, kommunikationsförmåga och
+        anpassningsförmåga, egenskaper som jag tror kommer vara till nytta som
+        utvecklare. Mitt hjärta klappar för webbgränssnitt, användarupplevelse
+        och pedagogik. Jag är en problemlösare med fokus på att hitta lösningar
+        och få saker gjorda. Min kompetens inom Frontend-utveckling inkluderar
+        HTML, CSS, Bootstrap, Sass, JavaScript, React, jQuery, GitHub, API:er,
+        Typescript, UX-design och agila arbetsmetoder. Dessutom har jag en
+        grundläggande förståelse för backend-utveckling. Jag är särskilt
+        passionerad när det gäller React, CSS och design och ser fram emot att
+        fortsätta utveckla mina färdigheter inom dessa områden.
       </p>
     </div>
   );
